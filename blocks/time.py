5ad047c9410b20ccbb00359a05a2d2c8083748ed
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-10 09:34:39 -0500

    Stuff

--- blocks/time.py
+++ blocks/time.py
@@ -1,4 +1,5 @@
 import datetime
 now = datetime.datetime.now()
 format = now.strftime('%I:%M %p')
+#print('<span foreground="blue">&#57522;</span><span background="blue" foreground="white">&#xf017;   ' + format + '</span>')
 print('&#xf017;   ' + format)
