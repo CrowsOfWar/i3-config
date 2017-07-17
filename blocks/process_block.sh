af5f7ccf5d1746438510e98efcc41da8d4270bf6
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-17 14:20:34 -0500

    No space between the blocks

--- blocks/process_block.sh
+++ blocks/process_block.sh
@@ -2,5 +2,5 @@
 bg_color="#FF0000"
 fg_color="#000000"
 var="$(python ~/.i3/blocks/$1.py)"
-echo "<span foreground='$bg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $var</span>"
+echo "<span foreground='$bg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $var </span>"
 #echo "<span foreground='$fg_color'>hello</span>"
