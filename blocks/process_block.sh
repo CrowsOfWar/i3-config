ad1a4e1bcdf674c818eb4a990e38d32e757bab4e
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-19 13:54:15 -0500

    try using important blocks

--- blocks/process_block.sh
+++ blocks/process_block.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
 
 bg_color="#073642"
 fg_color="#839496"
-var="$(python ~/.i3/blocks/$1.py)"
-echo "<span foreground='$bg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $var </span>"
-#echo "<span foreground='$fg_color'>hello</span>"
+output="$(python ~/.i3/blocks/$1.py)"
+
+formatted="<span foreground='$bg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $output </span>"
+
+if [[ $output == i* ]] ;
+then
+    formatted="important"
+fi
+
+echo $formatted
--- blocks/time.py
+++ blocks/time.py
@@ -2,4 +2,4 @@ import datetime
 now = datetime.datetime.now()
 format = now.strftime('%I:%M %p')
 #print('<span foreground="blue">&#57522;</span><span background="blue" foreground="white">&#xf017;   ' + format + '</span>')
-print('&#xf017;   ' + format)
+print('i&#xf017;   ' + format)
