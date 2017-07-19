79300dc738b5c3314e891ef9a71ebad26fb50947
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-19 14:02:23 -0500

    Fix important tag still showing

--- blocks/process_block.sh
+++ blocks/process_block.sh
@@ -8,6 +8,7 @@ if [[ $output == i* ]] ;
 then
     bg_color="#d33682"
     fg_color="#073642"
+    output=${output:1}
 fi
 
 formatted="<span foreground='$bg_color' background='$fg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $output </span>"
