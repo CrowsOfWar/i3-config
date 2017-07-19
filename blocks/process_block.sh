1c5c4e519e4c6b3050295c66db9bf7184f7f7d4c
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-19 13:56:35 -0500

    Important block coming along- has coloring

--- blocks/process_block.sh
+++ blocks/process_block.sh
@@ -4,11 +4,12 @@ bg_color="#073642"
 fg_color="#839496"
 output="$(python ~/.i3/blocks/$1.py)"
 
-formatted="<span foreground='$bg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $output </span>"
-
 if [[ $output == i* ]] ;
 then
-    formatted="important"
+    bg_color="#d33682"
+    fg_color="#073642"
 fi
+
+formatted="<span foreground='$bg_color' background='$fg_color'>&#57522;</span><span background='$bg_color' foreground='$fg_color'> $output </span>"
 
 echo $formatted
