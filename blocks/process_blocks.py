b77abce9e819192e73c3fab765c7f3c6e9cdb0ae
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-20 21:00:53 -0500

    It works

--- blocks/process_blocks.py
+++ blocks/process_blocks.py
@@ -5,7 +5,7 @@ scripts = [ 'date', 'time', 'ram' ]
 total_output = ''
 
 for script_name in scripts:
-    result = subprocess.check_output('python %s.py' % script_name, shell=True)
+    result = subprocess.check_output('python ~/.i3/blocks/%s.py' % script_name, shell=True)
     result_split = result.split('\n')
 
     output = result_split[0]
@@ -14,6 +14,6 @@ for script_name in scripts:
     bg_color = '#073642';
     fg_color = '#839496';
 
-    total_output += "<span foreground='%(bg_color)s' background='%(fg_color)s'>&#57522;</span><span background='%(bg_color)s' foreground='%(fg_color)s'> $output </span>" % { 'bg_color': bg_color, 'fg_color': fg_color }
+    total_output += "<span foreground='%(bg_color)s' background='%(fg_color)s'>&#57522;</span><span background='%(bg_color)s' foreground='%(fg_color)s'> %(output)s </span>" % { 'bg_color': bg_color, 'fg_color': fg_color, 'output': output }
 
 print(total_output)
