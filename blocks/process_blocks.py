d52b328936e31251fa1ad30bd0e8ec86f0e60ee6
CrowsOfWar <ofwarcrows@gmail.com>
2017-07-20 20:57:29 -0500

    Output works?

--- blocks/process_blocks.py
+++ blocks/process_blocks.py
@@ -2,8 +2,18 @@
 import subprocess
 
 scripts = [ 'date', 'time', 'ram' ]
+total_output = ''
 
 for script_name in scripts:
     result = subprocess.check_output('python %s.py' % script_name, shell=True)
     result_split = result.split('\n')
-    print('Ran %s.py, got %s' % (script_name, result))
+
+    output = result_split[0]
+    flags = '' if len(result_split) < 2 else result_split[1]
+
+    bg_color = '#073642';
+    fg_color = '#839496';
+
+    total_output += "<span foreground='%(bg_color)s' background='%(fg_color)s'>&#57522;</span><span background='%(bg_color)s' foreground='%(fg_color)s'> $output </span>" % { 'bg_color': bg_color, 'fg_color': fg_color }
+
+print(total_output)
